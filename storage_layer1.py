aa4af2e8605e701e5ae3d00410e0586b9d73c6a3
Virgil0203 <564347782@qq.com>
2020-11-19 17:21:04 +0800

    update

--- storage_layer1.py
+++ storage_layer1.py
@@ -7,6 +7,7 @@
 
 
 import json
+import os
 from xmlrpc.server import SimpleXMLRPCServer
 from socketserver import ThreadingMixIn
 
@@ -14,47 +15,78 @@ from socketserver import ThreadingMixIn
 class ThreadXMLRPCServer(ThreadingMixIn, SimpleXMLRPCServer):
     pass
 
-filename = 'data1.json'
-
 
 def get_k(key):
     """根据key查找读取数据"""
-    with open(filename, 'r') as f_obj:
-        datas = json.load(f_obj)
-        if key in datas.keys():
-            info = key + ': ' + datas[key]
-            return info
-        else:
-            info = "没有该关键字！"
-            return info
+    chunkNum = int(key) // 80000 + 1
+    chunkName = 'chunk_' + str(chunkNum) +'.json'
+    if os.path.exists(chunkName):
+        with open(chunkName, 'r') as f_obj:
+            datas = json.load(f_obj)
+            if key in datas.keys():
+                info = key + ': ' + datas[key]
+                return info
+            else:
+                info = "没有该关键字！"
+                return info
+    else:
+        info = "没有该关键字！"
+        return info
 
 
 def set_kv(key, value):
     """根据key，value插入或更新"""
-    with open(filename, 'r+') as f_obj:
-        datas = json.load(f_obj)
-        f_obj.seek(0, 0)
-        f_obj.truncate()
-        datas[key] = value
-        json.dump(datas, f_obj)
-        info = "成功更改！"
-        return info
+    chunkNum = int(key) // 80000 + 1
+    chunkName = 'chunk_' + str(chunkNum) +'.json'
+    if os.path.exists(chunkName):
+        with open(chunkName, 'r+') as f_obj:
+            datas = json.load(f_obj)
+            f_obj.seek(0, 0)
+            f_obj.truncate()
+            datas[key] = value
+            json.dump(datas, f_obj)
+            info = "成功更改！"
+            return info
+    else:
+        os.mknod(chunkName, 0o777)
+        textData = {'text': 'avoid error'} # 写入一个文本数据防止解析json出错
+        with open(chunkName, 'r+') as f_obj:
+            json.dump(textData, f_obj)
+
+        with open(chunkName, 'r+') as f_obj:
+            datas = json.load(f_obj)
+            f_obj.seek(0, 0)
+            f_obj.truncate()
+            datas[key] = value
+            json.dump(datas, f_obj)
+            info = "成功更改！"
+            return info
 
 
 def delete_k(key):
     """根据key删除对应的key value"""
-    with open(filename, 'r+') as f_obj:
-        datas = json.load(f_obj)
-        f_obj.seek(0, 0)
-        f_obj.truncate()
-        del_key = datas.pop(key)  # 返回删除的key
-        info = "已成功删除" + key + ": " + del_key
-        json.dump(datas, f_obj)
+    chunkNum = int(key) // 80000 + 1
+    chunkName = 'chunk_' + str(chunkNum) +'.json'
+    if os.path.exists(chunkName):
+        with open(chunkName, 'r+') as f_obj:
+            datas = json.load(f_obj)
+            if key in datas.keys():
+                f_obj.seek(0, 0)
+                f_obj.truncate()
+                del_key = datas.pop(key)  # 返回删除的key
+                info = "已成功删除" + key + ": " + del_key
+                json.dump(datas, f_obj)
+                return info
+            else:
+                info = "不存在！"
+                return info
+    else:
+        info = "不存在！"
         return info
 
 
 if __name__ == '__main__':
-    server1 = ThreadXMLRPCServer(('localhost', 8888)) # 初始化
+    server1 = ThreadXMLRPCServer(('192.168.18.128', 8888)) # 初始化
     server1.register_function(get_k, "get") # 注册函数
     server1.register_function(set_kv, "set") # 注册函数
     server1.register_function(delete_k, "delete") # 注册函数
