e99bbb682da70651888540a501cf81dc80ab9b38
Virgil0203 <564347782@qq.com>
2020-11-19 11:04:30 +0800

    add RPC

--- font_layer.py
+++ font_layer.py
@@ -1,7 +1,7 @@
 """
 这是前端层
 提供给用户使用接口
-包括关键字：get（读），set（写、更新），del（删除）
+包括关键字：get（读），set（写、更新），delete（删除）
 本层根据用户的键，根据哈希算法把值分别存储到不同的结点
 无限循环，反复询问用户操作，直到用户想退出
 """
@@ -11,7 +11,7 @@ import hash_route
 
 info = True
 while info:
-    keyword = input("请输入get或set或del（退出请键入q）: ")
+    keyword = input("请输入get或set或delete（退出请键入q）: ")
     if keyword == 'q':
         info = False
 
@@ -19,11 +19,10 @@ while info:
         key = input("\n请输入key:")
         hash_route.hash_get(key)
 
-
     if keyword == 'set':
         key, value = input('请输入key value，用空格隔开: ').split(' ')
         hash_route.hash_set(key, value)
 
-    if keyword == 'del':
+    if keyword == 'delete':
         key = input('请输入要删除的key: ')
         hash_route.hash_del(key)
